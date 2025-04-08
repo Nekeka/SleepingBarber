d2ea1fca49fc8b20522a47faa05d5c3f78e5f843
Mykyta Khomik <n.homick4@gmail.com>
2025-04-08 22:28:35 +0300

    Add project files.

---
+++ SleepingBarber/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SleepingBarber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
