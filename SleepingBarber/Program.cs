4059e07a2d14589bc5620677ab269630ac08ab52
Mykyta Khomik <n.homick4@gmail.com>
2025-04-11 17:21:20 +0300

    thread test

--- SleepingBarber/Program.cs
+++ SleepingBarber/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace SleepingBarber
+﻿using System.Threading;
+namespace SleepingBarber
+
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            //// создаем новые потоки
+            Thread myThread1 = new Thread(new ParameterizedThreadStart(Print));
+            Thread myThread2 = new Thread(Print);
+            Thread myThread3 = new Thread(message => Console.WriteLine(message));
+
+            // запускаем потоки
+            myThread1.Start("Hello");
+            myThread2.Start("Hi");
+            myThread3.Start("Salut");
+
+
+            void Print(object? message) => Console.WriteLine(message);
         }
     }
 }
